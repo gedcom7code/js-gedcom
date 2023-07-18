880126601ba42c91bd0a28d7814e45736a3caef1
Luther Tychonievich <tychonievich@gmail.com>
2023-07-18 09:46:36 -0400

    Add JSON serialization

--- gedc-parser.js
+++ gedc-parser.js
@@ -2,7 +2,7 @@
  * Parses GEDC data format strings into a forest of GEDC structures.
  * Allows additional validation for specific GEDC dialects.
  * 
- * Uses as liberal a GEDC dialect as I know how to code.
+ * Uses a generic dialect of GEDC.
  * 
  * Payloads that start "@" but not "@#" are encoded as "TAG @@"
  * thus, `TAG @#` and `TAG @@#` are synonyms for payload "@#"
@@ -11,7 +11,7 @@
  * Written by Luther Tychonievich, July 2023. Released to the public domain,
  * to be used freely for any purpose with or without acknowledgement.
  */
-//export { GEDCStruct, parseGEDC, g5ConfGEDC, g7ConfGEDC }
+export { GEDCStruct, g5ConfGEDC, g7ConfGEDC }
 
 /**
  * A GEDC structure:
@@ -39,11 +39,10 @@ class GEDCStruct {
     this.tag = tag
     this.#sup = sup
     if (sup instanceof GEDCStruct) sup.sub.push(this)
-    this.payload = ptr || str
     if (ptr instanceof GEDCStruct) {
       ptr.#ref.push(this)
       this.payload = ptr
-    } else if (ptr) {
+    } else if (ptr !== undefined) {
       this.ptr = ptr // temporary, will be removed by fixPtrs
     } else {
       this.payload = str
@@ -90,10 +89,11 @@ class GEDCStruct {
   /**
    * Serializes to GEDC string
    * 
+   * @param {string} newline - how lines should end; defaults to '\n'
    * @param {Number} maxlen - if positive, will wrap lines to this many characters
-   * @patam {string} newline - how lines should end; defaults to '\n'
+   * @param {boolean} escapes - if true, leading @# unchanged; if false, becomes @@#
    */
-  toString(maxlen, newline) {
+  toString(newline, maxlen, escapes) {
     if (!newline || !/^[\n\r]+$/.test(newline) ) newline = '\n'
     let level = 0
     for(let s = this.#sup; s; s = s.#sup) level += 1
@@ -103,7 +103,7 @@ class GEDCStruct {
     if (this.payload instanceof GEDCStruct) {
       ans += ` @${this.payload.#id}@`+newline
     } else if ('string' === typeof this.payload) {
-      let lines = this.payload.split(/\r\n?|\n\r?/g).map(s => s.replace(/^@(?!#)/,'@@'))
+      let lines = this.payload.split(/\r\n?|\n\r?/g).map(s => s.replace((escapes ? /^@(?!#)/ : /^@/),'@@'))
       let leader = ans
       ans += (lines[0].length > 0 ? ' ' : '')+(lines.join(newline+(level+1)+' CONT ')+newline).replaceAll('CONT '+newline, 'CONT'+newline)
       if (maxlen > 0 && ans.length > maxlen) {
@@ -115,143 +115,178 @@ class GEDCStruct {
         while (ipt.test(ans)) {
           ans = ans.replace(ipt, '$1'+pfx+'$2')
         }
-        ans = ans.replace(/^([0-9]+ CONC @(?!#))/gm, '$1@')
+        ans = ans.replace(escapes ? /^([0-9]+ CONC @(?!#))/gm : /^([0-9]+ CONC @)/gm, '$1@')
       }
     } else if (this.payload === null) {
       ans += ' @VOID@'+newline
     } else {
       ans += newline
     }
-    return ans + this.sub.map(s => s.toString(maxlen, newline)).join('')
+    return ans + this.sub.map(s => s.toString(newline, maxlen, escapes)).join('')
   }
-}
-
-/**
- * Parses a GEDC string into a list of top-level GEDC structures.
- *
- * Accepts a configuration object with the following keys:
- * - `len` = `0`{.js}
- * 
- *    positive: limit lines to this many characters
- * 
- *    zero: no length limit
- * 
- *    negative: no length limit and no CONC allowed
- *
- * - `tag` = `/.+/`{.js}
- *
- *    A regex to limit the set of permitted tags.
- *    Tags will always match at least `/^[^@\p{Cc}\p{Z}][^\p{Cc}\p{Z}]*$/u`{.js}:
- *    that is, 1 or more characters,
- *    no whitespace or control characters,
- *    and not beginning with `@`.
- *
- *  - `xref` = `/.+/`{.js}
- *
- *     A regex to limit the set of permitted cross-reference identifiers.
- *     Cross-reference identifiers will always match at least `/^([^@#\p{Cc}]|\t)([^@\p{Cc}]|\t)*$/u`{.js}:
- *     that is, one or more characters,
- *     no non-tab control characters,
- *     no `@`,
- *     and not beginning with `#`.
- *
- * - `linesep` = `/.+/`{.js}
- * 
- *     A regex to limit what is considered a line separation.
- *     Line separations will always match at least /^[\n\r]\p{WSpace}*$/u:
- *     that is, a carriage return or line feed
- *     followed by whitespace.
- *
- * - `delim` = `/.+/`{.js}
- *
- *     A regex to limit what is considered a delimiter.
- *     Delimiters will always match at least /^[ \t\p{Zs}]+$/u:
- *     that is, linear whitespace.
- *
- *     A single space will always be used during serialization, regardless of *     the value of `delim`.
- *
- * - `payload` = `/(.*)/`{.js}
- *
- *     A regex to limit permitted string payloads.
- *
- * 
- * @param {string} input - a full GEDC-serialized input
- * @param {Object} config - parser configuration; see above for more.
- * @param {function} logger - called with any error messages
- * @return {GEDCStructure[]}
- */
-function parseGEDC(input, config, logger) {
-  // level: [0-9]+
-  // delim: [ \t\p{Zs}]+
-  // xref: (?:[^@#\p{Cc}]|\t)(?:[^@\p{Cc}]|\t)*
-  // tag: [^@\p{Cc}\p{Z}][^\p{Cc}\p{Z}]*
-  // (level)(delim)(?:@(xref)@(delim))?(tag)(?:(delim)(?:@(xref)@|(payload)))?(?:(linesep)|$)
-  var lre = /([0-9]+)([ \t\p{Zs}]+)(?:@((?:[^@#\p{Cc}]|\t)(?:[^@\p{Cc}]|\t)*)@([ \t\p{Zs}]+))?([^@\p{Cc}\p{Z}][^\p{Cc}\p{Z}]*)(?:([ \t\p{Zs}]+)(?:@((?:[^@#\p{Cc}]|\t)(?:[^@\p{Cc}]|\t)*)@|((?:[^@\n\r]|@[@#])[^\n\r]*)))?(?:([\n\r]\p{WSpace}*)|$)/gu;
-  var context = [];
-  var records = [];
-  var ids = {};
-  var line = 0
-  var lastidx = 0
-  if (input.startsWith('\ufeff')) input = input.substr(1)
-  for(match of input.matchAll(lre)) {
-    line += 1
-    let [all,l,d1,i,d2,t,d3,x,p,n] = match
-    if (p && p.length > 1 && p[0] == '@' && p[1] == '@') p = p.substr(1)
-    if (logger) {
-      if (match.index != lastidx)
-        logger(`${line}: unable to parse ${JSON.stringify(match.input.substring(lastidx,match.index))}`)
-      if (config.delim) {
-        if (!config.delim.test(d1))
-          logger(`${line}: invalid delimiter ${JSON.stringify(d1)}`)
-        if (d2 !== undefined && !config.delim.test(d2))
-          logger(`${line}: invalid delimiter ${JSON.stringify(d2)}`)
-        if (d3 !== undefined && !config.delim.test(d3))
-          logger(`${line}: invalid delimiter ${JSON.stringify(d3)}`)
-      }
-      if (config.tag && !config.tag.test(t))
-        logger(`${line}: invalid tag ${JSON.stringify(t)}`)
-      if (config.xref) {
-        if (i !== undefined && !config.xref.test(i))
-          logger(`${line}: invalid xref_id ${JSON.stringify(i)}`)
-        if (x !== undefined && !config.xref.test(x))
-          logger(`${line}: invalid pointer ${JSON.stringify(x)}`)
-      }
-      if (config.linesep && n && !config.linesep.test(n))
-        logger(`${line}: invalid line separator ${JSON.stringify(n)}`)
-      if (config.payload && n && !config.payload.test(p))
-        logger(`${line}: invalid string payload ${JSON.stringify(p)}`)
-      if (config.len > 0 && all.length > config.len)
-        logger(`${line}: line has ${all.length} characters, but only ${config.len} permitted`)
-    }
-    lastidx = match.index + all.length
-    var level = Number(match[1]);
-    if (level > context.length) {
-      if (logger) logger(`${line}: level ${level} cannot follow level ${context.length-1}`)
-      continue
-    }
-    context.length = level
-    if (t == "CONT" || t == "CONC") {
-      if (context[level-1].payload instanceof GEDCStruct || 'ptr' in context[level-1]) {
-        if (logger) logger(`${line}: ${t} cannot follow pointer`)
-      } else if (context[level-1].sub.length > 0) {
-        if (logger) logger(`${line}: ${t} cannot follow substructure`)
-      }
-      if (context[level-1].payload === undefined) context[level-1].payload = ''
-      if (p === undefined) p = ''
-      if (t == 'CONT') context[level-1].payload += '\n' + p
-      else if ('len' in config && config.len < 0) {
-        if (logger) logger(`${line}: CONC not permitted`)
-      } else context[level-1].payload += p
-      continue;
-    }
-    let st = new GEDCStruct(t, level >= 0 ? context[level-1] : null, x, p, i)
-    context.push(st)
-    if (level == 0) records.push(st);
-    if (i) ids[i] = st;
+  
+  /**
+   * Encodes pointers using id and href
+   */
+  toJSON() {
+    const ans = {tag:this.tag}
+    if (this.#ref.length > 0) ans.id = this.#id
+    if (this.payload instanceof GEDCStruct)
+      ans.href = this.payload.#id
+    else if (this.payload === null)
+      ans.href = null
+    else if ('string' === typeof this.payload)
+      ans.text = this.payload
+    if (this.sub.length > 0) ans.sub = this.sub.map(e=>e.toJSON())
+    return ans
   }
-  new GEDCStruct('ADDED', records[1], records[0])
-  records.forEach(x => x.fixPtrs(ids, logger));
-  return records;
+  
+  /**
+   * Parses a GEDC string into a list of top-level GEDC structures.
+   *
+   * Accepts a configuration object with the following keys:
+   * - `len` = `0`{.js}
+   * 
+   *    positive: limit lines to this many characters
+   * 
+   *    zero: no length limit
+   * 
+   *    negative: no length limit and no CONC allowed
+   *
+   * - `tag` = `/.+/`{.js}
+   *
+   *    A regex to limit the set of permitted tags.
+   *    Tags will always match at least `/^[^@\p{Cc}\p{Z}][^\p{Cc}\p{Z}]*$/u`{.js}:
+   *    that is, 1 or more characters,
+   *    no whitespace or control characters,
+   *    and not beginning with `@`.
+   *
+   *  - `xref` = `/.+/`{.js}
+   *
+   *     A regex to limit the set of permitted cross-reference identifiers.
+   *     Cross-reference identifiers will always match at least `/^([^@#\p{Cc}]|\t)([^@\p{Cc}]|\t)*$/u`{.js}:
+   *     that is, one or more characters,
+   *     no non-tab control characters,
+   *     no `@`,
+   *     and not beginning with `#`.
+   *
+   * - `linesep` = `/.+/`{.js}
+   * 
+   *     A regex to limit what is considered a line separation.
+   *     Line separations will always match at least /^[\n\r]\p{WSpace}*$/u:
+   *     that is, a carriage return or line feed
+   *     followed by whitespace.
+   *
+   * - `delim` = `/.+/`{.js}
+   *
+   *     A regex to limit what is considered a delimiter.
+   *     Delimiters will always match at least /^[ \t\p{Zs}]+$/u:
+   *     that is, linear whitespace.
+   *
+   *     A single space will always be used during serialization, regardless of *     the value of `delim`.
+   *
+   * - `payload` = `/(.*)/`{.js}
+   *
+   *     A regex to limit permitted string payloads.
+   *
+   * - `zeros` = `true`{.js}
+   * 
+   *     If `false`, leading zeros on levels (like `02`) is not allowed
+   * 
+   * @param {string} input - a full GEDC-serialized input
+   * @param {Object} config - parser configuration; see above for more.
+   * @param {function} logger - called with any error messages
+   * @return {GEDCStructure[]}
+   */
+  static fromString(input, config, logger) {
+    // level: [0-9]+
+    // delim: [ \t\p{Zs}]+
+    // xref: (?:[^@#\p{Cc}]|\t)(?:[^@\p{Cc}]|\t)*
+    // tag: [^@\p{Cc}\p{Z}][^\p{Cc}\p{Z}]*
+    // (level)(delim)(?:@(xref)@(delim))?(tag)(?:(delim)(?:@(xref)@|(payload)))?(?:(linesep)|$)
+    var lre = /([0-9]+)([ \t\p{Zs}]+)(?:@((?:[^@#\p{Cc}]|\t)(?:[^@\p{Cc}]|\t)*)@([ \t\p{Zs}]+))?([^@\p{Cc}\p{Z}][^\p{Cc}\p{Z}]*)(?:([ \t\p{Zs}]+)(?:@((?:[^@#\p{Cc}]|\t)(?:[^@\p{Cc}]|\t)*)@|((?:[^@\n\r]|@[@#])[^\n\r]*)))?(?:([\n\r]\p{WSpace}*)|$)/gu;
+    var context = [];
+    var records = [];
+    var ids = {};
+    var line = 0
+    var lastidx = 0
+    if (input.startsWith('\ufeff')) input = input.substr(1)
+    for(let match of input.matchAll(lre)) {
+      line += 1
+      let [all,l,d1,i,d2,t,d3,x,p,n] = match
+      if (p && p.length > 1 && p[0] == '@' && p[1] == '@') p = p.substr(1)
+      if (logger) {
+        if (match.index != lastidx)
+          logger(`${line}: unable to parse ${JSON.stringify(match.input.substring(lastidx,match.index))}`)
+        if (!config.zeros && l.length > 1 && l[0] == '0')
+          logger(`${line}: leading zeros not permitted in level`)
+        if (config.delim) {
+          if (!config.delim.test(d1))
+            logger(`${line}: invalid delimiter ${JSON.stringify(d1)}`)
+          if (d2 !== undefined && !config.delim.test(d2))
+            logger(`${line}: invalid delimiter ${JSON.stringify(d2)}`)
+          if (d3 !== undefined && !config.delim.test(d3))
+            logger(`${line}: invalid delimiter ${JSON.stringify(d3)}`)
+        }
+        if (config.tag && !config.tag.test(t))
+          logger(`${line}: invalid tag ${JSON.stringify(t)}`)
+        if (config.xref) {
+          if (i !== undefined && !config.xref.test(i))
+            logger(`${line}: invalid xref_id ${JSON.stringify(i)}`)
+          if (x !== undefined && !config.xref.test(x))
+            logger(`${line}: invalid pointer ${JSON.stringify(x)}`)
+        }
+        if (config.linesep && n && !config.linesep.test(n))
+          logger(`${line}: invalid line separator ${JSON.stringify(n)}`)
+        if (config.payload && n && !config.payload.test(p))
+          logger(`${line}: invalid string payload ${JSON.stringify(p)}`)
+        if (config.len > 0 && all.length > config.len)
+          logger(`${line}: line has ${all.length} characters, but only ${config.len} permitted`)
+      }
+      lastidx = match.index + all.length
+      var level = Number(match[1]);
+      if (level > context.length) {
+        if (logger) logger(`${line}: level ${level} cannot follow level ${context.length-1}`)
+        continue
+      }
+      context.length = level
+      if (t == "CONT" || t == "CONC") {
+        if (context[level-1].payload instanceof GEDCStruct || 'ptr' in context[level-1]) {
+          if (logger) logger(`${line}: ${t} cannot follow pointer`)
+        } else if (context[level-1].sub.length > 0) {
+          if (logger) logger(`${line}: ${t} cannot follow substructure`)
+        }
+        if (context[level-1].payload === undefined) context[level-1].payload = ''
+        if (p === undefined) p = ''
+        if (t == 'CONT') context[level-1].payload += '\n' + p
+        else if ('len' in config && config.len < 0) {
+          if (logger) logger(`${line}: CONC not permitted`)
+        } else context[level-1].payload += p
+        continue;
+      }
+      let st = new GEDCStruct(t, level >= 0 ? context[level-1] : null, x, p, i)
+      context.push(st)
+      if (level == 0) records.push(st);
+      if (i) ids[i] = st;
+    }
+    records.forEach(x => x.fixPtrs(ids, logger));
+    return records;
+  }
+  
+  static fromJSON(o, ids, sup) {
+    const top = !ids
+    if (top) ids = {}
+    if (Array.isArray(o)) {
+      const recs = o.map(e => GEDCStruct.fromJSON(e, ids, sup))
+      if (top) recs.forEach(x => x.fixPtrs(ids))
+      return recs
+    }
+    const ans = new GEDCStruct(o.tag, sup, o.href, o.text, o.id)
+    if (o.id) ids[o.id] = ans
+    if (o.sub) o.sub.forEach(e => GEDCStruct.fromJSON(e, ids, ans))
+    if (top) ans.fixPtrs(ids)
+    return ans
+  }
 }
 
 /** GEDCOM 5.x-compatible configuration */
@@ -261,6 +296,8 @@ const g5ConfGEDC = {
   xref: /^[0-9a-z_A-Z][^\p{Cc}@]{0,19}$/u,
   linesep: /^[\r\n][\r\n \t]*$/,
   delim: /^ $/,
+  zeros: false,
+  escapes: true,
 }
 
 /** GEDCOM 7.x-compatible configuration */
@@ -271,14 +308,6 @@ const g7ConfGEDC = {
   linesep: /^(\r\n?|\n\r?)$/,
   delim: /^ $/,
   payload: /^.+$/,
+  zeros: false,
+  escapes: false,
 }
-
-console.log(parseGEDC(`0 HEAD
-1 GEDC
-2 VERS 7.0
-1 NOTE @#test@asdfghjklkjhg
-2 CONT @@fdsdfhjklkj@hgfdsfghjklkjhfdsdfghjkjhfds
-0 TRLR`, {}, console.log).map(e=>e.toString(20)).join(''))
-
-
-
