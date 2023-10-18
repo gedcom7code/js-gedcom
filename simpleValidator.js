6dc8e80df4fe27d4dc42fb93d6030b26c206aca6
Luther Tychonievich <tychonievich@gmail.com>
2023-10-18 13:40:27 -0500

    report fatal errors too

--- simpleValidator.js
+++ simpleValidator.js
@@ -23,19 +23,18 @@ window.validateFile = fileList => {
   var fr = new FileReader();
   fr.addEventListener('load', evt => {
     var data = evt.target.result
-    console.log(data)
     try {
       window.gedc = GEDCStruct.fromString(data, g7ConfGEDC, g7v.err)
+      if (!Array.isArray(window.gedc) || window.gedc.length == 0) {
+        addError("File does not appear to contain GEDCOM data")
+      }
+      if (document.getElementById('errors').childElementCount > 0) {
+        addWarning("Errors parsing syntax; version 7 structures not checked")
+      } else {
+        window.ged7 = G7Dataset.fromGEDC(gedc, g7v)
+      }
     } catch(error) {
-      addError(error.message)
-    }
-    if (!Array.isArray(window.gedc) || window.gedc.length == 0) {
-      addError("File does not appear to contain GEDCOM data")
-    }
-    if (document.getElementById('errors').childElementCount > 0) {
-      addWarning("Errors parsing syntax; version 7 structures not checked")
-    } else {
-      window.ged7 = G7Dataset.fromGEDC(gedc, g7v)
+      addError(error.message+"\nUnrecoverable error; parsing halted")
     }
   })
   fr.readAsText(fileList[0], 'utf-8')
